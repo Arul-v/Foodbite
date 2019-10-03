8503b0273f9f5f43985b87ca9b4c6febdefcb182
Gowtham <gowthamraj198@gmail.com>
2019-10-03 19:43:17 +0530

    Gowtham / Vidhya : added cross origin to hotels controller

--- src/main/java/com/foodbite/grabfood/controller/HotelsController.java
+++ src/main/java/com/foodbite/grabfood/controller/HotelsController.java
@@ -14,6 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "/")
 public class HotelsController {
